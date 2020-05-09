1f1522413750c8184cde5334e990325974cfff59
NihaalWagadia <nwagadia@gmail.com>
2020-05-09 17:26:09 -0400

    *

--- ValidPerfectSquare.java
+++ ValidPerfectSquare.java
@@ -2,23 +2,11 @@ class Solution {
     public boolean isPerfectSquare(int num) {
         if(num==2||num==3)return false;
         if(num==1)return true;
-        long val = 2;
-        Stack<Long>stack = new Stack<>();
-        while((val*val)<num){
-            val *= val;
-            stack.push(val);
+        long i=2;
+        while((i*i)<num){
+            i++;
         }
-        long end = stack.isEmpty()?4:stack.pop();
-        long init = stack.isEmpty()?2:stack.pop();
-        for(long i = init; i<=end; i++){
-          long mul = i*i;
-            if(mul == num){
-                return true;
-            }
-            else if(mul>num){
-                return false;
-            }
-        }
+        if(i*i==num)return true;
         return false;
     }
 }
